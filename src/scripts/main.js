d465bc96d5e67b2df5c1acc6cf369b0b2c310404
morfeii <morfeii88@gmail.com>
2020-02-06 14:51:48 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello mate academy!');
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', () => {
+  event.preventDefault();
+
+  const thumbnail = event.target.closest('a');
+
+  if (!thumbnail) {
+    return;
+  }
+
+  largeImg.src = thumbnail.href;
+});
